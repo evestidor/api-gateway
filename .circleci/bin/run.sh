7dcc6b2bcedbcff327c63291181d513fcf724815
Tiago Lira <tiagoliradsantos@gmail.com>
2019-07-09 23:09:33 -0300

    Moving back to circleci folder

--- .circleci/bin/run.sh
+++ .circleci/bin/run.sh
@@ -2,18 +2,15 @@
 set -e
 
 # VARS ---------------------------------------------------------------------------
-ORGANIZATION_NAME=$(head -n 1 conf/vars/organization_name)
-PROJECT_NAME=$(head -n 1 conf/vars/project_name)
+ORGANIZATION_NAME=$(head -n 1 .circleci/vars/organization_name)
+PROJECT_NAME=$(head -n 1 .circleci/vars/project_name)
 
 # IMAGE INFO ----------------------------------------------------------------------
 IMAGE_NAME=$ORGANIZATION_NAME/$PROJECT_NAME
 
 # GIT INFO ------------------------------------------------------------------------
-LATEST_COMMIT=$(git rev-parse HEAD)
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 CLEANED_BRANCH_NAME=${BRANCH_NAME//\//-}  # Slashes can't go through
 
-# BUILD ---------------------------------------------------------------------------
-docker build -t $IMAGE_NAME:$CLEANED_BRANCH_NAME .
-docker build -t $IMAGE_NAME:$CLEANED_BRANCH_NAME-$LATEST_COMMIT .
-echo $IMAGE_NAME:$CLEANED_BRANCH_NAME
+# RUN ---------------------------------------------------------------------------
+docker run -it $IMAGE_NAME:$CLEANED_BRANCH_NAME $@
